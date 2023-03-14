d7b043a267b2829efb0bfad52b5260adf4331750
Sebastien Gault <sgault.webdev@gmail.com>
2023-03-14 16:46:08 +0100

    added loader and some error handling improvement on login

--- src/pages/ErrorDisplay.jsx
+++ src/pages/ErrorDisplay.jsx
@@ -2,7 +2,7 @@ export default function ErrorDisplay({ code=null, message=''}) {
     return (
         <main className="main bg-dark">
             <div className="error">
-                <h2>{code ? code : 'Erreur de l\'application'}</h2>
+                <h2>{code ? code : 'Erreur du Serveur'}</h2>
                 <p>{code ? message : 'Veuillez recharger la page'}</p>
             </div>
         </main>
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { authLogin } from '../app/middlewares'
+import Loader from '../components/Loader'
 import ErrorDisplay from './ErrorDisplay'
 
 
@@ -33,47 +34,50 @@ export default function LogIn() {
     }
 
     return (
-        <React.Fragment>
-            {auth.error ?
-                <ErrorDisplay /> :
-                <main className='main bg-dark'>
-                    <section className='sign-in-content'>
-                        <i className='fa fa-user-circle sign-in-icon'></i>
-                        <h1>Sign In</h1>
-                        <form
-                            onSubmit={handleFormSubmit}
-                        >
-                            <div className='input-wrapper'>
-                                <label htmlFor='username'>Username</label>
-                                <input
-                                    type='text'
-                                    id='username'
-                                    onChange={(e) => { setUserName(e.target.value) }}
-                                />
-                            </div>
-                            <div className='input-wrapper'>
-                                <label htmlFor='password'>Password</label>
-                                <input
-                                    type='password'
-                                    id='password'
-                                    onChange={(e) => { setPassword(e.target.value) }}
-                                />
-                            </div>
-                            <div className='input-remember'>
-                                <input
-                                    type='checkbox'
-                                    id='remember-me'
-                                    onChange={(e) => { setRememberMe(e.target.checked) }}
-                                />
-                                <label htmlFor='remember-me'>Remember me</label>
-                            </div>
+        <main className='main bg-dark'>
+            {auth.status === 'fetching' ?
+                <Loader /> :
+                <section className='sign-in-content'>
+                    <i className='fa fa-user-circle sign-in-icon'></i>
+                    <h1>Sign In</h1>
+                    {auth.error &&
+                        <div className='error-message'>
+                            <span>{auth.error.code === 400 ? 'Identifiants invalides !' : 'Erreur du serveur'}</span>
+                        </div>
+                    }
+                    <form
+                        onSubmit={handleFormSubmit}
+                    >
+                        <div className='input-wrapper'>
+                            <label htmlFor='username'>Username</label>
+                            <input
+                                type='text'
+                                id='username'
+                                onChange={(e) => { setUserName(e.target.value) }}
+                            />
+                        </div>
+                        <div className='input-wrapper'>
+                            <label htmlFor='password'>Password</label>
+                            <input
+                                type='password'
+                                id='password'
+                                onChange={(e) => { setPassword(e.target.value) }}
+                            />
+                        </div>
+                        <div className='input-remember'>
+                            <input
+                                type='checkbox'
+                                id='remember-me'
+                                onChange={(e) => { setRememberMe(e.target.checked) }}
+                            />
+                            <label htmlFor='remember-me'>Remember me</label>
+                        </div>
 
-                            <button type='submit' className='sign-in-button'>Sign In</button>
-                        </form>
-                    </section>
-                </main>
+                        <button type='submit' className='sign-in-button'>Sign In</button>
+                    </form>
+                </section>
             }
-        </React.Fragment>
 
+        </main>
     )
 }
